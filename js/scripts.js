79affc4975557d85304c7011409b786c6977a482
Tung Nguyen <tongueroo@gmail.com>
2022-01-24 18:42:52 +0000

    update edit link with js to work with caching

--- js/scripts.js
+++ js/scripts.js
@@ -81,4 +81,10 @@ $(document).ready(function() {
     }
   })
 
+  // adjust edit-page link
+  var github_base = $('#edit-page').attr('href');
+  var md_page = window.location.pathname.replace('/','_').replace(/\/$/,'.md');
+  var href = github_base + md_page;
+  $('#edit-page').attr('href', href);
+
 });
